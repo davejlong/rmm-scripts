f0a5635a471c950e3e9a1a191163bc8b8a7a9189
Dave Long <dlong@cagedata.com>
2025-09-18 12:45:18 -0400

    WIP: Add PDF association script

---
+++ Windows/Set-PDFAssociation.ps1
@@ -0,0 +1,49 @@
+<#
+.DESCRIPTION
+WIP: Sets what app should be used to open PDFs by default.
+#>
+
+###
+# Script Settings
+# Set variables here that will be populated by the RMM or set when running manually
+###
+
+$PDFApps = @{
+  Acrobat = "Acrobat.Document.DC"
+  FoxitEditor = "FoxitPhantomPDF.Document"
+  Edge = "MSEdgePDF"
+}
+
+$ScriptSettings = @{
+  UploadToSyncro = $false
+  App = $PDFApps.Acrobat
+}
+
+$ScriptName = ([System.IO.FileInfo]$PSCommandPath).BaseName
+$OutFile = Join-Path $env:TEMP "$(Get-Date -Format FileDate)-$ScriptName.txt"
+
+Write-Host "Running $ScriptName with params:"
+Write-Host $ScriptSettings
+Write-Host "==========================="
+
+###
+# Execution Logic
+# Put the logic for the script here.
+###
+
+$Command = @(
+  "assoc"
+  ".pdf=$($ScriptSettings.App)"
+)
+Start-Process -Wait -Passthru -FilePath cmd.exe -ArgumentList $Command
+
+###
+# RMM Processing
+###
+if ($env:SyncroModule -and $ScriptSettings.UploadToSyncro) {
+  Import-Module $env:SyncroModule
+
+  Write-Host "Uploading to Syncro"
+
+  Upload-File -FilePath $OutFile
+}
